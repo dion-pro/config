184a734da38ef72614b68d0b4795a1885b05b297
dion-pro <104863902+dion-pro@users.noreply.github.com>
2023-10-21 16:44:20 +0200

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+alias vim='nvim'
+export EDITOR='gnome-text-editor'
+
+set -gx PATH $PATH /home/dion/.cargo/bin
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
